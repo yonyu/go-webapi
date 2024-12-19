2d03e49fe0923ab2f16c822d140a199fb803c0a1
yyu <ypyu@hotmail.com>
2024-12-18 22:22:34 -0800

    Initial entrypoint for the go program

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// Run - is going to be responsible for the instantiation
+// and startup of the Go application
+func Run() error {
+	fmt.Println("Starting up the application")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest Web API")
+	if err := Run(); err != nil {
+		fmt.Println(err)
+	}
 }
